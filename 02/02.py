f3190312541837c35d49b5beeca439d0d5b4c120
Paul Reiter <reiter.paul@gmail.com>
2020-12-02 22:22:54 +0100

    Refactor solution for day 2

--- 02/02.py
+++ 02/02.py
@@ -1,8 +1,3 @@
-
-with open("input.txt", "r") as stream:
-    puzzle_input = [line for line in stream.read().strip().split("\n")]
-
-
 def parse_line(line):
     count_interval, letter, password = line.split()
     count_interval = (int(c) for c in count_interval.split("-"))
@@ -10,32 +5,31 @@ def parse_line(line):
 
 
 def is_password_valid(line):
-    count_interval, letter, password = parse_line(line)
-    min_, max_ = count_interval
+    (min_, max_), letter, password = parse_line(line)
     return min_ <= password.count(letter) <= max_
 
 
 def is_password_actually_valid(line):
-    valid_positions, letter, password = parse_line(line)
-    pos1, pos2 = valid_positions
-    return (password[pos1-1] == letter) ^ (password[pos2-1] == letter)
+    (pos1, pos2), letter, password = parse_line(line)
+    return (password[pos1 - 1] == letter) ^ (password[pos2 - 1] == letter)
 
 
-# part 1
+if __name__ == "__main__":
+    with open("input.txt", "r") as stream:
+        puzzle_input = stream.read().strip().split("\n")
 
-num_valid_passwords = 0
-for line in puzzle_input:
-    if is_password_valid(line):
-        num_valid_passwords += 1
+    # part 1
+    num_valid_passwords = 0
+    for line in puzzle_input:
+        if is_password_valid(line):
+            num_valid_passwords += 1
 
-print(f"The number of valid passwords is: {num_valid_passwords}")
+    print(f"The number of valid passwords is: {num_valid_passwords}")
 
+    # part 2
+    num_valid_passwords = 0
+    for line in puzzle_input:
+        if is_password_actually_valid(line):
+            num_valid_passwords += 1
 
-# part 2
-
-num_valid_passwords = 0
-for line in puzzle_input:
-    if is_password_actually_valid(line):
-        num_valid_passwords += 1
-
-print(f"The number of actual valid passwords is: {num_valid_passwords}")
+    print(f"The number of actual valid passwords is: {num_valid_passwords}")
