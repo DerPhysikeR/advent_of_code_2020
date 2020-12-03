e25fd718d393ed947b77b7f4784b1d8023b078d4
Paul Reiter <reiter.paul@gmail.com>
2020-12-03 18:34:00 +0100

    Refactor solution for part 1 to allow multiple walks

--- 03/03.py
+++ 03/03.py
@@ -6,13 +6,17 @@ def read_tree_pattern(file_):
         return stream.read().strip().split("\n")
 
 
-if __name__ == "__main__":
-    tree_pattern = read_tree_pattern(argv[-1])
-    d_row, d_col = 1, 3
+def walk_tree_pattern(tree_pattern, direction):
+    d_col, d_row = direction
     tree_count = 0
     row, col = 0, 0
     while row < len(tree_pattern):
         if tree_pattern[row][col % len(tree_pattern[0])] == "#":
             tree_count += 1
         row, col = row + d_row, col + d_col
-    print(tree_count)
+    return tree_count
+
+
+if __name__ == "__main__":
+    tree_pattern = read_tree_pattern(argv[-1])
+    print(walk_tree_pattern(tree_pattern, (3, 1)))
