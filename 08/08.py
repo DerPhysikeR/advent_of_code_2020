c4f982eeaed0b36929f3d4ce56a898c6c72d3047
Paul Reiter <reiter.paul@gmail.com>
2020-12-08 10:13:56 +0100

    Solve part 2 of day 8

--- 08/08.py
+++ 08/08.py
@@ -1,5 +1,6 @@
 from sys import argv
 
+
 def read_instructions(filename):
     with open(filename) as stream:
         instructions = stream.read().strip().split("\n")
@@ -41,7 +42,27 @@ class Emulator:
         return cls(read_instructions(filename))
 
 
+def does_program_terminate(emulator):
+    visited_line_numbers = set()
+    for line_number, instruction in emulator:
+        if line_number in visited_line_numbers:
+            break
+        visited_line_numbers.add(line_number)
+    else:
+        return True
+    return False
+
+
+def instruction_replacer(instructions, to_replace, replacement):
+    for i, instruction in enumerate(instructions):
+        if to_replace in instruction:
+            new_instructions = instructions[:]
+            new_instructions[i] = new_instructions[i].replace(to_replace, replacement)
+            yield new_instructions
+
+
 if __name__ == "__main__":
+    # part 1
     emulator = Emulator.from_file(argv[-1])
     visited_line_numbers, accumulator = set(), 0
     for line_number, instruction in emulator:
@@ -52,3 +73,13 @@ if __name__ == "__main__":
         print(f"{line_number}: {instruction}")
     print(f"The accumulator had the value {accumulator} before the programm repeated.")
 
+    # part 2
+    original_instructions = read_instructions(argv[-1])
+    for instructions in instruction_replacer(original_instructions, "jmp", "nop"):
+        emulator = Emulator(instructions)
+        if does_program_terminate(emulator):
+            print(emulator.accumulator)
+    for instructions in instruction_replacer(original_instructions, "nop", "jmp"):
+        emulator = Emulator(instructions)
+        if does_program_terminate(emulator):
+            print(emulator.accumulator)
