a9812c111e62cb22ca7230c860beee283eeae16d
Paul Reiter <reiter.paul@gmail.com>
2020-12-11 18:27:31 +0100

    Solve part 2 of day 11

--- 11/11.py
+++ 11/11.py
@@ -1,3 +1,4 @@
+from itertools import product
 from sys import argv
 
 
@@ -48,6 +49,27 @@ def evolve_seat_layout(
     return result
 
 
+def count_visibly_occupied_neighbors(seat_layout, row, col):
+    n_rows, n_cols = len(seat_layout), len(seat_layout[0])
+    sum_ = 0
+    for drow, dcol in product([-1, 0, 1], [-1, 0, 1]):
+        if drow == dcol == 0:
+            continue
+        r, c = row + drow, col + dcol
+        while 0 <= r < n_rows and 0 <= c < n_cols:
+            if seat_layout[r][c] == "L":
+                break
+            if seat_layout[r][c] == "#":
+                sum_ += 1
+                break
+            r, c = r + drow, c + dcol
+    return sum_
+
+
+def evolve_seat_layout_by_visibility(seat_layout):
+    return evolve_seat_layout(seat_layout, count_visibly_occupied_neighbors, 5)
+
+
 def to_string(seat_layout):
     return "\n".join("".join(row) for row in seat_layout)
 
@@ -64,8 +86,19 @@ def print_evolution(seat_layout, evolution_function):
 
 if __name__ == "__main__":
     original_seat_layout = read_seat_layout(argv[-1])
-    seat_layout = print_evolution(original_seat_layout, evolve_seat_layout)
 
+    # part 1
+    seat_layout = print_evolution(original_seat_layout, evolve_seat_layout)
+    print(
+        f"The final seat layout has {sum(r.count('#') for r in seat_layout)} occupied seats."
+    )
+
+    breakpoint()
+
+    # part 2
+    seat_layout = print_evolution(
+        original_seat_layout, evolve_seat_layout_by_visibility
+    )
     print(
         f"The final seat layout has {sum(r.count('#') for r in seat_layout)} occupied seats."
     )
