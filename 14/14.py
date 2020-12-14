b93d829596c5b030560fad621a60a69695223639
Paul Reiter <reiter.paul@gmail.com>
2020-12-14 17:58:15 +0100

    Refactoring for derived ActualPortComputer

--- 14/14.py
+++ 14/14.py
@@ -7,29 +7,23 @@ def read_program(filename):
 
 
 class PortComputer:
-
-    MAX_VALUE_36bit = 68719476735
-
     def __init__(self):
         self.memory = {}
-        self.force_on_mask = 0
-        self.force_off_mask = self.MAX_VALUE_36bit
+        self.mask = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
 
     def run_line(self, line):
         if line.startswith("mask"):
-            self._update_mask(line.partition(" = ")[2])
+            self.mask = line.partition(" = ")[2]
         elif line.startswith("mem"):
             address, _, value = line.partition("] = ")
             self._write_to_memory(int(address[4:]), int(value))
         else:
             raise ValueError("Invalid command `{line}`.")
 
-    def _update_mask(self, string_mask):
-        self.force_on_mask = int(string_mask.replace("X", "0"), 2)
-        self.force_off_mask = int(string_mask.replace("X", "1"), 2)
-
     def _write_to_memory(self, address, value):
-        self.memory[address] = (value | self.force_on_mask) & self.force_off_mask
+        force_on_mask = int(self.mask.replace("X", "0"), 2)
+        force_off_mask = int(self.mask.replace("X", "1"), 2)
+        self.memory[address] = (value | force_on_mask) & force_off_mask
 
     def sum_values(self):
         return sum(value for value in self.memory.values())
